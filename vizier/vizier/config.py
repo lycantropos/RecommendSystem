9b246f29680d7c7fdd9590d866f6b6d24e372184
ace <azatibrakov@gmail.com>
2017-02-28 21:24:44 +0700

    Removing redundant configurations

--- vizier/vizier/config.py
+++ vizier/vizier/config.py
@@ -1,16 +1,3 @@
-import os
-
-MAX_SCORE = 5
-MIN_SCORE = 0
-
-DATA_DIR = 'db'
-RATINGS_DATABASE_NAME = 'ratings.csv'
-FILMS_DATABASE_NAME = 'movies.txt'
-FILMS_WITH_PLOT_DATABASE_NAME = 'movies_full.csv'
-LINKS_FILE_NAME = 'links.csv'
-
-BASE_DIR = os.path.dirname(__file__)
-
 PACKAGE = 'vizier'
 CONFIG_DIR_NAME = 'configurations'
 LOGGING_CONF_FILE_NAME = 'logging.conf'
